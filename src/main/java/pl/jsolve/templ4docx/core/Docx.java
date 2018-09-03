005df67eb60bc1730a3e9557e95a38f9b6c9b483
Farrukh Ijaz <ijazfx@gmail.com>
2018-09-04 03:12:39 +0500

    Support for variable replacement in header, footer and footnote, and save as PDF

--- src/main/java/pl/jsolve/templ4docx/core/Docx.java
+++ src/main/java/pl/jsolve/templ4docx/core/Docx.java
@@ -1,5 +1,7 @@
 package pl.jsolve.templ4docx.core;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -10,6 +12,8 @@ import java.util.List;
 
 import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.docx4j.Docx4J;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
 import pl.jsolve.sweetener.io.Resources;
 import pl.jsolve.templ4docx.cleaner.DocumentCleaner;
@@ -21,164 +25,199 @@ import pl.jsolve.templ4docx.variable.Variables;
 
 /**
  * The main class responsible for reading docx template, finding variables, saving completed docx template
- * @author Lukasz Stypka
+ * @author Lukasz Stypka, Farrukh Ijaz
  */
 public class Docx implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private VariablePattern variablePattern = new VariablePattern("${", "}");
+	private VariablePattern variablePattern = new VariablePattern("${", "}");
 
-    /**
-     * Path to .docx template file
-     */
-    private String docxPath;
+	/**
+	 * Path to .docx template file
+	 */
+	private String docxPath;
 
-    /**
-     * XWPFDocument object with opened .docx template file
-     */
-    private XWPFDocument docx = null;
+	/**
+	 * XWPFDocument object with opened .docx template file
+	 */
+	private XWPFDocument docx = null;
 
-    /**
-     * Document cleaner service, which is used to merge split variable
-     */
-    private DocumentCleaner documentCleaner;
+	/**
+	 * Document cleaner service, which is used to merge split variable
+	 */
+	private DocumentCleaner documentCleaner;
 
-    /**
-     * Document meta-information processor, which is used to mark occurrences
-     * of variables for possibility of updating them at any time.
-     */
-    private DocumentMetaProcessor documentMetaProcessor;
+	/**
+	 * Document meta-information processor, which is used to mark occurrences
+	 * of variables for possibility of updating them at any time.
+	 */
+	private DocumentMetaProcessor documentMetaProcessor;
 
-    /**
-     * If true, all variables occurrences in document will be marked for
-     * possibility of updating them at any time.
-     */
-    private boolean processMetaInformation = false;
+	/**
+	 * If true, all variables occurrences in document will be marked for
+	 * possibility of updating them at any time.
+	 */
+	private boolean processMetaInformation = false;
 
-    public Docx(String docxPath) {
-        this.docxPath = docxPath;
-        open();
-        this.documentCleaner = new DocumentCleaner();
-        this.documentMetaProcessor = new DocumentMetaProcessor();
-    }
+	public Docx(String docxPath) {
+		this.docxPath = docxPath;
+		open();
+		this.documentCleaner = new DocumentCleaner();
+		this.documentMetaProcessor = new DocumentMetaProcessor();
+	}
 
-    public Docx(InputStream docxInputStream) {
-        open(docxInputStream);
-        this.documentCleaner = new DocumentCleaner();
-        this.documentMetaProcessor = new DocumentMetaProcessor();
-    }
+	public Docx(InputStream docxInputStream) {
+		open(docxInputStream);
+		this.documentCleaner = new DocumentCleaner();
+		this.documentMetaProcessor = new DocumentMetaProcessor();
+	}
 
-    /**
-     * Open .docx template file and create new object of XWPFDocument class
-     */
-    private void open() {
-        try {
-            open(new FileInputStream(docxPath));
-        } catch (FileNotFoundException ex) {
-            throw new OpenDocxException(ex.getMessage(), ex.getCause());
-        }
-    }
+	/**
+	 * Open .docx template file and create new object of XWPFDocument class
+	 */
+	private void open() {
+		try {
+			open(new FileInputStream(docxPath));
+		} catch (FileNotFoundException ex) {
+			throw new OpenDocxException(ex.getMessage(), ex.getCause());
+		}
+	}
 
-    /**
-     * Read .docx template file from an input stream and create an instance of XWPFDocument class
-     */
-    private void open(InputStream inputStream) {
-        try {
-            docx = new XWPFDocument(inputStream);
-        } catch (Exception ex) {
-            throw new OpenDocxException(ex.getMessage(), ex.getCause());
-        }
-    }
+	/**
+	 * Read .docx template file from an input stream and create an instance of XWPFDocument class
+	 */
+	private void open(InputStream inputStream) {
+		try {
+			docx = new XWPFDocument(inputStream);
+		} catch (Exception ex) {
+			throw new OpenDocxException(ex.getMessage(), ex.getCause());
+		}
+	}
 
-    /**
-     * Finds list of variables in the template file which satisfy the variable pattern
-     * @return list of found variables
-     */
-    public List<String> findVariables() {
-        VariablesExtractor extractor = new VariablesExtractor();
-        String content = readTextContent();
-        return extractor.extract(content, variablePattern);
-    }
+	/**
+	 * Finds list of variables in the template file which satisfy the variable pattern
+	 * @return list of found variables
+	 */
+	public List<String> findVariables() {
+		VariablesExtractor extractor = new VariablesExtractor();
+		String content = readTextContent();
+		return extractor.extract(content, variablePattern);
+	}
 
-    /**
-     * Returns text content as String
-     * @return String text content of template file
-     */
-    public String readTextContent() {
-        XWPFWordExtractor extractor = null;
-        try {
-            extractor = new XWPFWordExtractor(docx);
-            return extractor.getText();
-        } finally {
-            if (extractor != null) {
-                Resources.closeStream(extractor);
-            }
-        }
-    }
+	/**
+	 * Returns text content as String
+	 * @return String text content of template file
+	 */
+	public String readTextContent() {
+		XWPFWordExtractor extractor = null;
+		try {
+			extractor = new XWPFWordExtractor(docx);
+			return extractor.getText();
+		} finally {
+			if (extractor != null) {
+				Resources.closeStream(extractor);
+			}
+		}
+	}
 
-    /**
-     * Method replaces found variable to value provided by user
-     * @param variables Variables
-     */
-    public void fillTemplate(Variables variables) {
-        documentCleaner.clean(this, variables, variablePattern);
-        if (processMetaInformation)
-            documentMetaProcessor.processMetaInformation(this, variables, variablePattern);
-        DocumentExecutor documentExecutor = new DocumentExecutor(variables);
-        documentExecutor.execute(this);
-    }
+	/**
+	 * Method replaces found variable to value provided by user
+	 * @param variables Variables
+	 */
+	public void fillTemplate(Variables variables) {
+		documentCleaner.clean(this, variables, variablePattern);
+		if (processMetaInformation)
+			documentMetaProcessor.processMetaInformation(this, variables, variablePattern);
+		DocumentExecutor documentExecutor = new DocumentExecutor(variables);
+		documentExecutor.execute(this);
+	}
 
-    /**
-     * Save filled template as new .docx file
-     * @param outputPath String
-     */
-    public void save(String outputPath) {
-        try {
-            docx.write(new FileOutputStream(outputPath));
-        } catch (Exception ex) {
-            throw new OpenDocxException(ex.getMessage(), ex.getCause());
-        }
-    }
+	/**
+	 * Save filled template as new .docx file
+	 * @param outputPath String
+	 */
+	public void save(String outputPath) {
+		try {
+			docx.write(new FileOutputStream(outputPath));
+		} catch (Exception ex) {
+			throw new OpenDocxException(ex.getMessage(), ex.getCause());
+		}
+	}
 
-    /**
-     * Save filled template as new .docx file
-     * @param outputStream OutputStream
-     */
-    public void save(OutputStream outputStream) {
-        try {
-            docx.write(outputStream);
-        } catch (Exception ex) {
-            throw new OpenDocxException(ex.getMessage(), ex.getCause());
-        }
-    }
+	/**
+	 * Save filled template as new .docx file
+	 * @param outputStream OutputStream
+	 */
+	public void save(OutputStream outputStream) {
+		try {
+			docx.write(outputStream);
+		} catch (Exception ex) {
+			throw new OpenDocxException(ex.getMessage(), ex.getCause());
+		}
+	}
 
-    /**
-     * Set variable pattern which indicates how the variable looks like
-     * @param variablePattern VariablePattern
-     */
-    public void setVariablePattern(VariablePattern variablePattern) {
-        this.variablePattern = variablePattern;
-    }
+	/**
+	 * Save filled template as new .pdf file
+	 * @param outputPath String
+	 */
+	public void saveToPdf(String outputPath) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			docx.write(baos);
+			baos.close();
+			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+			WordprocessingMLPackage wordprocessingMLPackage = WordprocessingMLPackage.load(bais);
+			Docx4J.toPDF(wordprocessingMLPackage, new FileOutputStream(outputPath));
+		} catch (Exception ex) {
+			throw new OpenDocxException(ex.getMessage(), ex.getCause());
+		}
+	}
 
-    /**
-     * Returns opened XWPFDocument
-     * @return XWPFDocument
-     */
-    public XWPFDocument getXWPFDocument() {
-        return docx;
-    }
+	/**
+	 * Save filled template as new .pdf file
+	 * @param outputStream OutputStream
+	 */
+	public void saveToPdf(OutputStream outputStream) {
+		try {
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			docx.write(baos);
+			baos.close();
+			ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+			WordprocessingMLPackage wordprocessingMLPackage = WordprocessingMLPackage.load(bais);
+			Docx4J.toPDF(wordprocessingMLPackage, outputStream);
+		} catch (Exception ex) {
+			throw new OpenDocxException(ex.getMessage(), ex.getCause());
+		}
+	}
 
-    public boolean isProcessMetaInformation() {
-        return processMetaInformation;
-    }
+	/**
+	 * Set variable pattern which indicates how the variable looks like
+	 * @param variablePattern VariablePattern
+	 */
+	public void setVariablePattern(VariablePattern variablePattern) {
+		this.variablePattern = variablePattern;
+	}
 
-    public void setProcessMetaInformation(boolean processMetaInformation) {
-        this.processMetaInformation = processMetaInformation;
+	/**
+	 * Returns opened XWPFDocument
+	 * @return XWPFDocument
+	 */
+	public XWPFDocument getXWPFDocument() {
+		return docx;
+	}
 
-    public VariablePattern getVariablePattern() {
-        return variablePattern;
+	public boolean isProcessMetaInformation() {
+		return processMetaInformation;
+	}
 
-    }
+	public void setProcessMetaInformation(boolean processMetaInformation) {
+		this.processMetaInformation = processMetaInformation;
+	}
+
+	public VariablePattern getVariablePattern() {
+		return variablePattern;
+
+	}
 
 }
